9bade0d34ff753b990c614f5b41be0b6930d0512
Kohei Tanaka <kohei69@gmail.com>
2023-08-11 16:13:49 +0900

    update setup

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,10 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   moduleFileExtensions: ['js', 'ts'],
   transform: {
     '^.+\\.ts$': 'ts-jest',
+    '^.+\\.jsx?$': 'babel-jest',
     ".*\\.(vue)$": "@vue/vue3-jest",
   },
   transformIgnorePatterns: [
--- tests/AppAlert.spec.ts
+++ tests/AppAlert.spec.ts
@@ -2,6 +2,7 @@ import { mount } from '@vue/test-utils';
 import AppAlert from '../components/AppAlert.vue';
 
 describe('AppAlert', () => {
+
   it('matches snapshot', () => {
     const wrapper = mount(AppAlert);
     const image = wrapper.html();
